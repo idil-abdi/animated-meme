e131ac61384598949a33a539f35a3eeb60c7aad6
idil-abdi <75079587+idil-abdi@users.noreply.github.com>
2024-10-04 15:25:44 +0000

    l100 day python

---
+++ 100-day-python/day-one.py
@@ -0,0 +1,6 @@
+############ Day One: ############
+###### Printing, Commenting, Debugging, String Maniplution and Varibles
+print('Welcome to the Band Name Generator.')
+city = input("what's the name of the city you grew up in?\n ")
+pet = input("what's your pet name?\n ")
+print(f"Your band name could be {city} {pet}")
